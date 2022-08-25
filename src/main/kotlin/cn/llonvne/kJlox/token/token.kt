f458fab923daeb1411469ec5924c3a583357d1f6
Llonvne <work@llonvne.cn>
2022-08-26 05:44:39 +0800

    美化了 Token 格式

--- src/main/kotlin/cn/llonvne/kJlox/token/token.kt
+++ src/main/kotlin/cn/llonvne/kJlox/token/token.kt
@@ -5,29 +5,55 @@ private const val unPrintableTokenStr = "\u0000";
 enum class TokenType(val value: String?) {
 
     // Single-character tokens.
-    LEFT_PAREN("("), RIGHT_PAREN(")"), LEFT_BRACE("{"), RIGHT_BRACE("}"), COMMA(","), DOT("."), MINUS("-"), PLUS("+"), SEMICOLON(
-        ";"
-    ),
-    SLASH("/"), STAR("*"),
+    LEFT_PAREN("("),
+    RIGHT_PAREN(")"),
+    LEFT_BRACE("{"),
+    RIGHT_BRACE("}"),
+    COMMA(","),
+    DOT("."),
+    MINUS("-"),
+    PLUS("+"),
+    SEMICOLON(";"),
+    SLASH("/"),
+    STAR("*"),
 
     // One or two character tokens.
-    BANG("!"), BANG_EQUAL("!="), EQUAL("="), EQUAL_EQUAL("=="), GREATER(">"), GREATER_EQUAL(">="), LESS("<"), LESS_EQUAL(
-        "<="
-    ),
+    BANG("!"),
+    BANG_EQUAL("!="),
+    EQUAL("="),
+    EQUAL_EQUAL("=="),
+    GREATER(">"),
+    GREATER_EQUAL(">="),
+    LESS("<"),
+    LESS_EQUAL("<="),
 
     // Literals.
-    IDENTIFIER(unPrintableTokenStr), STRING(unPrintableTokenStr), NUMBER(unPrintableTokenStr),
+    IDENTIFIER(unPrintableTokenStr),
+    STRING(unPrintableTokenStr),
+    NUMBER(unPrintableTokenStr),
 
     // Keywords.
-    AND("and"), CLASS("class"), ELSE("else"), FALSE("false"), FUN("fun"), FOR("for"), IF("if"), NIL("nil"), OR("or"), PRINT(
-        "print"
-    ),
-    RETURN("return"), SUPER("super"), THIS("this"), TRUE("true"), VAR("var"), WHILE("while"),
+    AND("and"),
+    CLASS("class"),
+    ELSE("else"),
+    FALSE("false"),
+    FUN("fun"),
+    FOR("for"),
+    IF("if"),
+    NIL("nil"),
+    OR("or"),
+    PRINT("print"),
+    RETURN("return"),
+    SUPER("super"),
+    THIS("this"),
+    TRUE("true"),
+    VAR("var"),
+    WHILE("while"),
 
     EOF(null);
 }
 
-val printableTokenList: Map<String, TokenType> by lazy {
+val printableTokenMap: Map<String, TokenType> by lazy {
     val map = mutableMapOf<String, TokenType>()
     for (v in TokenType.values()) {
         if (v.value == null || v.value == "\u0000") {
